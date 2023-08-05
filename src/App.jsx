c610315a2c39418a82bdb7867586882bf66908e3
yulelily <124838371+yulelily@users.noreply.github.com>
2023-08-05 17:27:22 -0400

    Add images

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import pokeFns from './pokeFns.js'
 import Pokemon from './Pokemon.jsx'
+import data from './assets/data.js'
 import './App.css'
 
 export default function App() {
@@ -12,6 +13,8 @@ export default function App() {
       <h1>pokimane memory game !!!!!</h1>
       <button onClick={async () => await pokeFns(setPokemons)} >start</button>
       <Pokemon pokemons={pokemons} />
+      <img src={data[93].img} alt={data[93].name} ></img>
+      <img src={data[94].img} alt={data[94].name} ></img>
     </>
   )
 }
---
+++ src/assets/data.js
@@ -0,0 +1,608 @@
+const pokeArray = [
+  {
+    "name": "Turtwig",
+    "img": "../images/turtwig.png"
+  },
+  {
+    "name": "Grotle",
+    "img": "../images/grotle.png"
+  },
+  {
+    "name": "Torterra",
+    "img": "../images/torterra.png"
+  },
+  {
+    "name": "Chimchar",
+    "img": "../images/chimchar.png"
+  },
+  {
+    "name": "Monferno",
+    "img": "../images/monferno.png"
+  },
+  {
+    "name": "Infernape",
+    "img": "../images/infernape.png"
+  },
+  {
+    "name": "Piplup",
+    "img": "../images/piplup.png"
+  },
+  {
+    "name": "Prinplup",
+    "img": "../images/prinplup.png"
+  },
+  {
+    "name": "Empoleon",
+    "img": "../images/empoleon.png"
+  },
+  {
+    "name": "Starly",
+    "img": "../images/starly.png"
+  },
+  {
+    "name": "Staravia",
+    "img": "../images/staravia.png"
+  },
+  {
+    "name": "Staraptor",
+    "img": "../images/staraptor.png"
+  },
+  {
+    "name": "Bidoof",
+    "img": "../images/bidoof.png"
+  },
+  {
+    "name": "Bibarel",
+    "img": "../images/bibarel.png"
+  },
+  {
+    "name": "Kricketot",
+    "img": "../images/kricketot.png"
+  },
+  {
+    "name": "Kricketune",
+    "img": "../images/kricketune.png"
+  },
+  {
+    "name": "Shinx",
+    "img": "../images/shinx.png"
+  },
+  {
+    "name": "Luxio",
+    "img": "../images/luxio.png"
+  },
+  {
+    "name": "Luxray",
+    "img": "../images/luxray.png"
+  },
+  {
+    "name": "Abra",
+    "img": "../images/abra.png"
+  },
+  {
+    "name": "Kadabra",
+    "img": "../images/kadabra.png"
+  },
+  {
+    "name": "Alakazam",
+    "img": "../images/alakazam.png"
+  },
+  {
+    "name": "Magikarp",
+    "img": "../images/magikarp.png"
+  },
+  {
+    "name": "Gyarados",
+    "img": "../images/gyarados.png"
+  },
+  {
+    "name": "Budew",
+    "img": "../images/budew.png"
+  },
+  {
+    "name": "Roselia",
+    "img": "../images/roselia.png"
+  },
+  {
+    "name": "Roserade",
+    "img": "../images/roserade.png"
+  },
+  {
+    "name": "Zubat",
+    "img": "../images/zubat.png"
+  },
+  {
+    "name": "Golbat",
+    "img": "../images/golbat.png"
+  },
+  {
+    "name": "Crobat",
+    "img": "../images/crobat.png"
+  },
+  {
+    "name": "Geodude",
+    "img": "../images/geodude.png"
+  },
+  {
+    "name": "Graveler",
+    "img": "../images/graveler.png"
+  },
+  {
+    "name": "Golem",
+    "img": "../images/golem.png"
+  },
+  {
+    "name": "Onix",
+    "img": "../images/onix.png"
+  },
+  {
+    "name": "Steelix",
+    "img": "../images/steelix.png"
+  },
+  {
+    "name": "Cranidos",
+    "img": "../images/cranidos.png"
+  },
+  {
+    "name": "Rampardos",
+    "img": "../images/rampardos.png"
+  },
+  {
+    "name": "Shieldon",
+    "img": "../images/shieldon.png"
+  },
+  {
+    "name": "Bastiodon",
+    "img": "../images/bastiodon.png"
+  },
+  {
+    "name": "Machop",
+    "img": "../images/machop.png"
+  },
+  {
+    "name": "Machoke",
+    "img": "../images/machoke.png"
+  },
+  {
+    "name": "Machamp",
+    "img": "../images/machamp.png"
+  },
+  {
+    "name": "Psyduck",
+    "img": "../images/psyduck.png"
+  },
+  {
+    "name": "Golduck",
+    "img": "../images/golduck.png"
+  },
+  {
+    "name": "Burmy",
+    "img": "../images/burmy.png"
+  },
+  {
+    "name": "Wormadam",
+    "img": "../images/wormadam.png"
+  },
+  {
+    "name": "Mothim",
+    "img": "../images/mothim.png"
+  },
+  {
+    "name": "Wurmple",
+    "img": "../images/wurmple.png"
+  },
+  {
+    "name": "Silcoon",
+    "img": "../images/silcoon.png"
+  },
+  {
+    "name": "Beautifly",
+    "img": "../images/beautifly.png"
+  },
+  {
+    "name": "Cascoon",
+    "img": "../images/cascoon.png"
+  },
+  {
+    "name": "Dustox",
+    "img": "../images/dustox.png"
+  },
+  {
+    "name": "Combee",
+    "img": "../images/combee.png"
+  },
+  {
+    "name": "Vespiquen",
+    "img": "../images/vespiquen.png"
+  },
+  {
+    "name": "Pachirisu",
+    "img": "../images/pachirisu.png"
+  },
+  {
+    "name": "Buizel",
+    "img": "../images/buizel.png"
+  },
+  {
+    "name": "Floatzel",
+    "img": "../images/floatzel.png"
+  },
+  {
+    "name": "Cherubi",
+    "img": "../images/cherubi.png"
+  },
+  {
+    "name": "Cherrim",
+    "img": "../images/cherrim.png"
+  },
+  {
+    "name": "Shellos",
+    "img": "../images/shellos.png"
+  },
+  {
+    "name": "Gastrodon",
+    "img": "../images/gastrodon.png"
+  },
+  {
+    "name": "Heracross",
+    "img": "../images/heracross.png"
+  },
+  {
+    "name": "Aipom",
+    "img": "../images/aipom.png"
+  },
+  {
+    "name": "Ambipom",
+    "img": "../images/ambipom.png"
+  },
+  {
+    "name": "Drifloon",
+    "img": "../images/drifloon.png"
+  },
+  {
+    "name": "Drifblim",
+    "img": "../images/drifblim.png"
+  },
+  {
+    "name": "Buneary",
+    "img": "../images/buneary.png"
+  },
+  {
+    "name": "Lopunny",
+    "img": "../images/lopunny.png"
+  },
+  {
+    "name": "Gastly",
+    "img": "../images/gastly.png"
+  },
+  {
+    "name": "Haunter",
+    "img": "../images/haunter.png"
+  },
+  {
+    "name": "Gengar",
+    "img": "../images/gengar.png"
+  },
+  {
+    "name": "Misdreavus",
+    "img": "../images/misdreavus.png"
+  },
+  {
+    "name": "Mismagius",
+    "img": "../images/mismagius.png"
+  },
+  {
+    "name": "Murkrow",
+    "img": "../images/murkrow.png"
+  },
+  {
+    "name": "Honchkrow",
+    "img": "../images/honchkrow.png"
+  },
+  {
+    "name": "Glameow",
+    "img": "../images/glameow.png"
+  },
+  {
+    "name": "Purugly",
+    "img": "../images/purugly.png"
+  },
+  {
+    "name": "Goldeen",
+    "img": "../images/goldeen.png"
+  },
+  {
+    "name": "Seaking",
+    "img": "../images/seaking.png"
+  },
+  {
+    "name": "Barboach",
+    "img": "../images/barboach.png"
+  },
+  {
+    "name": "Whiscash",
+    "img": "../images/whiscash.png"
+  },
+  {
+    "name": "Chingling",
+    "img": "../images/chingling.png"
+  },
+  {
+    "name": "Chimecho",
+    "img": "../images/chimecho.png"
+  },
+  {
+    "name": "Stunky",
+    "img": "../images/stunky.png"
+  },
+  {
+    "name": "Skuntank",
+    "img": "../images/skuntank.png"
+  },
+  {
+    "name": "Meditite",
+    "img": "../images/meditite.png"
+  },
+  {
+    "name": "Medicham",
+    "img": "../images/medicham.png"
+  },
+  {
+    "name": "Bronzor",
+    "img": "../images/bronzor.png"
+  },
+  {
+    "name": "Bronzong",
+    "img": "../images/bronzong.png"
+  },
+  {
+    "name": "Ponyta",
+    "img": "../images/ponyta.png"
+  },
+  {
+    "name": "Rapidash",
+    "img": "../images/rapidash.png"
+  },
+  {
+    "name": "Bonsly",
+    "img": "../images/bonsly.png"
+  },
+  {
+    "name": "Sudowoodo",
+    "img": "../images/sudowoodo.png"
+  },
+  {
+    "name": "Mine jr.",
+    "img": "../images/mime-jr.png"
+  },
+  {
+    "name": "Mr. Mime",
+    "img": "../images/mr-mime.png"
+  },
+  {
+    "name": "Happiny",
+    "img": "../images/happiny.png"
+  },
+  {
+    "name": "Chansey",
+    "img": "../images/chansey.png"
+  },
+  {
+    "name": "Blissey",
+    "img": "../images/blissey.png"
+  },
+  {
+    "name": "Cleffa",
+    "img": "../images/cleffa.png"
+  },
+  {
+    "name": "Clefairy",
+    "img": "../images/clefairy.png"
+  },
+  {
+    "name": "Clefable",
+    "img": "../images/clefable.png"
+  },
+  {
+    "name": "Chatot",
+    "img": "../images/chatot.png"
+  },
+  {
+    "name": "Pichu",
+    "img": "../images/pichu.png"
+  },
+  {
+    "name": "Pikachu",
+    "img": "../images/pikachu.png"
+  },
+  {
+    "name": "Raichu",
+    "img": "../images/raichu.png"
+  },
+  {
+    "name": "Hoothoot",
+    "img": "../images/hoothoot.png"
+  },
+  {
+    "name": "Noctowl",
+    "img": "../images/noctowl.png"
+  },
+  {
+    "name": "Spiritomb",
+    "img": "../images/spiritomb.png"
+  },
+  {
+    "name": "Gible",
+    "img": "../images/gible.png"
+  },
+  {
+    "name": "Gabite",
+    "img": "../images/gabite.png"
+  },
+  {
+    "name": "Garchomp",
+    "img": "../images/garchomp.png"
+  },
+  {
+    "name": "Munchlax",
+    "img": "../images/munchlax.png"
+  },
+  {
+    "name": "Snorlax",
+    "img": "../images/snorlax.png"
+  },
+  {
+    "name": "Unown",
+    "img": "../images/unown.png"
+  },
+  {
+    "name": "Riolu",
+    "img": "../images/riolu.png"
+  },
+  {
+    "name": "Lucario",
+    "img": "../images/lucario.png"
+  },
+  {
+    "name": "Wooper",
+    "img": "../images/wooper.png"
+  },
+  {
+    "name": "Quagsire",
+    "img": "../images/quagsire.png"
+  },
+  {
+    "name": "Wingull",
+    "img": "../images/wingull.png"
+  },
+  {
+    "name": "Pelipper",
+    "img": "../images/pelipper.png"
+  },
+  {
+    "name": "Girafarig",
+    "img": "../images/girafarig.png"
+  },
+  {
+    "name": "Hippopotas",
+    "img": "../images/hippopotas.png"
+  },
+  {
+    "name": "Hippowdon",
+    "img": "../images/hippowdon.png"
+  },
+  {
+    "name": "Azurill",
+    "img": "../images/azurill.png"
+  },
+  {
+    "name": "Marill",
+    "img": "../images/marill.png"
+  },
+  {
+    "name": "Azumarill",
+    "img": "../images/azumarill.png"
+  },
+  {
+    "name": "Skorupi",
+    "img": "../images/skorupi.png"
+  },
+  {
+    "name": "Drapion",
+    "img": "../images/drapion.png"
+  },
+  {
+    "name": "Croagunk",
+    "img": "../images/croagunk.png"
+  },
+  {
+    "name": "Toxicroak",
+    "img": "../images/toxicroak.png"
+  },
+  {
+    "name": "Carnivine",
+    "img": "../images/carnivine.png"
+  },
+  {
+    "name": "Remoraid",
+    "img": "../images/remoraid.png"
+  },
+  {
+    "name": "Octillery",
+    "img": "../images/octillery.png"
+  },
+  {
+    "name": "Finneon",
+    "img": "../images/finneon.png"
+  },
+  {
+    "name": "Lumineon",
+    "img": "../images/lumineon.png"
+  },
+  {
+    "name": "Tentacool",
+    "img": "../images/tentacool.png"
+  },
+  {
+    "name": "Tentacruel",
+    "img": "../images/tentacruel.png"
+  },
+  {
+    "name": "Feebas",
+    "img": "../images/feebas.png"
+  },
+  {
+    "name": "Milotic",
+    "img": "../images/milotic.png"
+  },
+  {
+    "name": "Mantyke",
+    "img": "../images/mantyke.png"
+  },
+  {
+    "name": "Mantine",
+    "img": "../images/mantine.png"
+  },
+  {
+    "name": "Snover",
+    "img": "../images/snover.png"
+  },
+  {
+    "name": "Abomasnow",
+    "img": "../images/abomasnow.png"
+  },
+  {
+    "name": "Sneasel",
+    "img": "../images/sneasel.png"
+  },
+  {
+    "name": "Weavile",
+    "img": "../images/weavile.png"
+  },
+  {
+    "name": "Uxie",
+    "img": "../images/uxie.png"
+  },
+  {
+    "name": "Mesprit",
+    "img": "../images/mesprit.png"
+  },
+  {
+    "name": "Azelf",
+    "img": "../images/azelf.png"
+  },
+  {
+    "name": "Dialga",
+    "img": "../images/dialga.png"
+  },
+  {
+    "name": "Palkia",
+    "img": "../images/palkia.png"
+  },
+  {
+    "name": "Manaphy",
+    "img": "../images/manaphy.png"
+  }
+]
+
+export default pokeArray;
