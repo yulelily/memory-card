d54a47f2a45a33eca29a949454e2674a9bde4c2f
yulelily <124838371+yulelily@users.noreply.github.com>
2023-08-06 14:25:22 -0400

    Implement memory mechanic

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,30 @@ export default function App() {
     setPokemons(newPokedex);
   }
 
+  function handleClick(pokemonName) {
+
+    if (!memory.has(pokemonName)) {
+      memory.add(pokemonName);
+      setMemory(memory);
+      setCorrect(correct + 1);
+
+      if (correct + 1 >= pokemons.length) {
+        console.log("win !");
+        // win !
+      } else {
+        shufflePokedex();
+      }
+
+    } else {
+      setMemory(new Set());
+      setCorrect(0);
+      // game over
+    }
+  }
+
   const [pokemons, setPokemons] = useState([]);
+  const [memory, setMemory] = useState(new Set());
+  const [correct, setCorrect] = useState(0);
 
   return (
     <>
@@ -28,9 +51,12 @@ export default function App() {
         <button value={24} onClick={(e) => pokeFns.populatePokedex(setPokemons, e.target.value)} >Hard</button>
         <button value={36} onClick={(e) => pokeFns.populatePokedex(setPokemons, e.target.value)} >Maddening</button>
       </div>
+      <div>
+        <p>correct: {correct}</p>
+      </div>
       <div className="gameBoard" >
         {pokemons.map(pokemon => 
-        <div className="pokeCard" key={pokemon.id} onClick={shufflePokedex} >
+        <div className="pokeCard" key={pokemon.id} value={pokemon.name} onClick={(e) => handleClick(e.currentTarget.getAttribute("value"))} >
           <Pokemon pokemon={pokemon} />
         </div>)}
       </div>
