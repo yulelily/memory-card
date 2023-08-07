fa3a0cdde70e6dcbb3b75274368f7e0b27768445
yulelily <124838371+yulelily@users.noreply.github.com>
2023-08-07 13:27:47 -0400

    Link GitHub Repository

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,6 @@ import './styles/index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <button className="linkGithub" onClick={() => {window.open("https://github.com/yulelily/memory-card")}} >yulelily</button>
   </React.StrictMode>,
 )
